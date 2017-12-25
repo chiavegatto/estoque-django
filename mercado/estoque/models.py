619c3f45d5f75bc31cf6dbf4e5df4209d9e4b5b7
vipkry <lolpsxvipkry2@gmail.com>
2017-12-25 14:21:56 -0200

    model 'produto' created

--- mercado/estoque/models.py
+++ mercado/estoque/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.nome
 
