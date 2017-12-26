6c88e52790c04bc3886fb1296f0283282d1132b0
vipkry <lolpsxvipkry2@gmail.com>
2017-12-25 23:43:47 -0200

    building user product page

--- mercado/estoque/views.py
+++ mercado/estoque/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from estoque.models import Produto
 
 def index(request):
     return render(request, 'estoque/home.html')
+
+def produtos(request):
+    produto_list = Produto.objects.all().order_by("nome")
+    return render(request, 'estoque/produtos.html', {'produto_list': produto_list})
+
+def produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    return render(request, 'estoque/produto_single.html', {'produto': produto})
