8599f67202bfa19fffc659f260fc0ef632ea997a
vipkry <lolpsxvipkry2@gmail.com>
2017-12-26 14:35:40 -0200

    buying is now possible, still missing some functions though

--- mercado/estoque/forms.py
+++ mercado/estoque/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Produto
 
 class AddProdutoForm(forms.Form):
     nome = forms.CharField(label='Nome do novo produto', max_length=300)
+
+class CompraLevaProdutosForm(forms.Form):
+    produto = forms.ModelChoiceField(queryset=Produto.objects.all(), empty_label="Selecione um produto")
+    quantidade = forms.IntegerField(min_value=1)
+    valor = forms.DecimalField(decimal_places=2)
+    
--- mercado/estoque/views.py
+++ mercado/estoque/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from estoque.models import Produto
-from .forms import AddProdutoForm
+from estoque.models import Produto, Compra
+from .forms import AddProdutoForm, CompraLevaProdutosForm
 from django.db.models import Sum
+import logging
+
 
 def index(request):
     return render(request, 'estoque/home.html')
@@ -15,12 +17,23 @@ def produtos(request):
             produto_novo = Produto(nome=nome_novo)
             produto_novo.save()
 
-
     form = AddProdutoForm()
     produto_list = Produto.objects.all().order_by("nome").annotate(estoque=Sum('compra__quantidade'))
-    
-    
+
     return render(request, 'estoque/produtos.html', {'produto_list': produto_list, 'form': form})
 
 def compra(request):
-    return render(request, 'estoque/compra.html')
+    if request.method == 'POST':
+        #recebeu requisição de compra
+        form = CompraLevaProdutosForm(request.POST)
+        if form.is_valid():
+            produto = form.cleaned_data['produto']
+            quantidade = form.cleaned_data['quantidade'] 
+            # multiplicando por 100 pois armazenamos com BigInt e não com decimal (sem perder a representação dos centavos)
+            valor = int(float(form.cleaned_data['valor'] * 100))
+            compra = Compra(quantidade=quantidade, valor=valor, produto=produto, valor_medio=1)
+            compra.save()
+
+
+    form = CompraLevaProdutosForm()
+    return render(request, 'estoque/compra.html', {'form': form})
