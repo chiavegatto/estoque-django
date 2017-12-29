e35df8400a14b6ef2a0c86ef727396dedc506734
vipkry <lolpsxvipkry2@gmail.com>
2017-12-29 02:25:17 -0200

    removed valor_medio from produto admin

--- mercado/estoque/admin.py
+++ mercado/estoque/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from estoque.models import Produto, Compra
 
-admin.site.register(Produto)
-
-# @admin.register(Compra)
-# class CompraAdmin(admin.ModelAdmin):
-#   exclude = ('valor_medio',)
-#   list_display = ('produto','quantidade','valor', 'valor_medio')
-#   list_filter = ('produto','valor', 'quantidade', 'valor_medio')
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+  exclude = ('valor_medio',)
 
 admin.site.register(Compra)
